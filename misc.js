c722087cd03bb225e24485289e44669333ef3402
Florian Meyer <fsjmeyer@gmail.com>
2018-10-14 13:26:40 +0200

    grid

---
+++ misc.js
@@ -0,0 +1,28 @@
+/* var birthday = new Date('Thu Mar 05 1998 23:00:00 GMT+0100 (W. Europe Standard Time)');
+var date = Date(Date.now());
+a = date.toString();
+var statement = "";
+
+var dif = date-birthday
+
+window.document.write("<p>Innerhalb des JavaScripts</p>" + birthday + " - " + y;
+
+switch (dif) {
+    case (dif % 10 == 1):
+    statement = "st day alive";
+    break;
+
+    case (dif % 10 == 2):
+    statement = "nd day alive";
+    break;
+
+    case (dif % 10 == 3):
+    statement = "rd day alive";
+    break;
+
+    case (dif % 10 > 3):
+    statement = "th day alive";
+    break;
+}
+
+window.document.write("This is your " + dif + statement) */
--- script.js
+++ script.js
@@ -8,32 +8,5 @@ var bday = new Date ('05 Mar 1998 23:00')
 var y = Math.round(today / days);
 var x = Math.round(bday.getTime() / days)
 var dif = y-x
-document.getElementById("demo").innerHTML = "<p>Tag </p>" + dif;
-/* var birthday = new Date('Thu Mar 05 1998 23:00:00 GMT+0100 (W. Europe Standard Time)');
-var date = Date(Date.now());
-a = date.toString();
-var statement = "";
 
-var dif = date-birthday
-
-window.document.write("<p>Innerhalb des JavaScripts</p>" + birthday + " - " + y;
-
-switch (dif) {
-    case (dif % 10 == 1):
-    statement = "st day alive";
-    break;
-
-    case (dif % 10 == 2):
-    statement = "nd day alive";
-    break;
-
-    case (dif % 10 == 3):
-    statement = "rd day alive";
-    break;
-
-    case (dif % 10 > 3):
-    statement = "th day alive";
-    break;
-}
-
-window.document.write("This is your " + dif + statement) */
+document.getElementById("demo").innerHTML = dif;
